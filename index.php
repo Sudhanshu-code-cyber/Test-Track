c6c12159fa53208d6e09b8b4da0171a4338856a2
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2025-01-23 23:17:16 +0530

    complete tailwindcss

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="src/output.css" rel="stylesheet">
 </head>
-<body>
-    hello
+<body class="bg-red-500 ">
+    
 </body>
 </html>

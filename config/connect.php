0e406b29aa8333f895959c6b84eae2a5b59bc0c3
theaman82 <amankumaraman55555@gmail.com>
2025-01-28 09:13:55 +0530

    some login authentication

--- config/connect.php
+++ config/connect.php
@@ -1,6 +1,6 @@
 <?php
     $connect = new mysqli("localhost", "root", "", "testtrack") or die("error connecting to db");
-
+    session_start();
     // alert message function
     function msg($msg){
         echo "<script>alert('$msg')</script>";
@@ -8,5 +8,20 @@
 
     function redirect($page){
         echo "<script>window.open('$page', '_self')</script>";
+    }
+
+    function redirectIfNotAuth(){
+        if(!isset($_SESSION['user'])){
+            redirect("login.php");
+        }
+    }
+
+    function getUser(): array{
+        $email = $_SESSION['user'];
+        global $connect;
+        $query = $connect->query("select * from users where email='$email'");
+
+        $userData = $query->fetch_assoc();
+        return $userData;
     }
 ?>
